ebd0ae1f6bac306e1b914a5cf8b5678c681fbe4f
AhmedEMokhtar <ahmed.mokhtar@arab-security.com>
2024-11-28 00:31:25 +0200

    learing advanded course"

---
+++ advanced_course/lessons/1_external_file.js
@@ -0,0 +1,17 @@
+
+document.getElementById("div_btn").addEventListener("click", () => {
+    const n1 = document.getElementById("first_number").value;
+    const n2 = document.getElementById("second_number").value;
+    const result = document.getElementById("result");
+    result.innerHTML = `Result: ` + divion(Number(n1), Number(n2));
+});
+
+
+function divion(n1, n2) {
+    if (n2 == 0) {
+        alert("can not divided on zero")
+    }
+    else {
+        return n1 / n2;
+    }
+}
